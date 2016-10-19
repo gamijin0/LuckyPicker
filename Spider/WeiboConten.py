c35eb21f438c2283146e8160c1b28b981ba25953
SleepyBadMan <1187692525@qq.com>
2016-10-19 08:28:58 +0800

    commit for spider gui fan

--- Spider/WeiboConten.py
+++ Spider/WeiboConten.py
@@ -1,30 +1,37 @@
 # -*- coding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
-
+#百度百科首页url
 url = 'https://baike.baidu.com/'
-#payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.get(url, params=None)
 
+r = requests.get(url, params=None)
+#必须转换编码
 r.encoding='utf-8'
 
 soup=BeautifulSoup(r.text,"html.parser")
-
 
 #pTitle=soup.findAll('div',class_="content_tit")
 
 pContent=soup.findAll('div',class_="content_cnt")
 
+#将结果存储在文件中
+#url.get_text()为一条新闻，str；
+import os
 
-# p=[pContent,pTitle]
+filepath = "./Result"
+if (os.path.exists(filepath) == False):
+    os.mkdir(filepath)
+# 文件名
+import datetime
 
-# print(pContent)
+filename = "weiboContent[%s].txt" % (str(datetime.datetime.now())[0:10])
+# 存储html
+with open(os.path.join(filepath, filename), 'w') as f:
+    for url in pContent:
+        if "词条" not in url.get_text() and "兑换" not in url.get_text() and "任务" not in url.get_text() and "商城" not in url.get_text():
+            print(url.get_text())
+            f.write(url.get_text())
+            f.write('\n ')
+            #print(isinstance(url.get_text(),str))
 
-for url in pContent:
-    # print(url)
-    if "词条"not in url.get_text()and "兑换"not in url.get_text()and "任务" not in url.get_text()and "商城"not in url.get_text():
-        print(url.get_text())
-    # print(url.get('a'))
-    # if (url.get('a')==None):
-    #     print(url.get_text())
-#listUrls=soup.findAll()
+
