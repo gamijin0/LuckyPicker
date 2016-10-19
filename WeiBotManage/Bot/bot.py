302b35ba075b83ecebd67dfcb346a0a0df480918
chaos <497386856@qq.com>
2016-10-19 18:59:55 +0800

    try to fix decode bug od utf-8 str

--- WeiBotManage/Bot/bot.py
+++ WeiBotManage/Bot/bot.py
@@ -15,18 +15,18 @@ class Bot:
 
     #if use requests###################
     session = requests.session()
-    headers = eval("{'Connection': 'keep-alive', 'Cookie': {'_T_WM': 'd84a71d49bc7c7fb49b88edf731f55a3', 'SUHB': '0k1yFH5UJVChAi', 'M_WEIBOCN_PARAMS': 'uicode%3D20000174', 'SCF': 'Alhi3iuZKNjYbnL77hWoMvkQoE4edClB0S5n_r6NteS2u6p08-Xjk7DnXr_Em6P9tglZ1FZUg-UySu_l2XM-Ex8.', 'SUB': '_2A256-08sDeTxGeVP71sX8CjOwz6IHXVWBFFkrDV6PUJbkdBeLWLlkW02jdHGyTIL9-KM1daXjNac-RdsbA..', 'SSOLoginState': '1476345724'}, 'Accept-Encoding': 'gzip,deflate', 'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'User-Agent': 'Mozilla/5.0(X11;Linuxx86_64;rv', 'Upgrade-Insecure-Requests': '1', 'Host': 'm.weibo.cn'}")
+    headers = eval(u"{'Connection': 'keep-alive', 'Cookie': {'_T_WM': 'd84a71d49bc7c7fb49b88edf731f55a3', 'SUHB': '0k1yFH5UJVChAi', 'M_WEIBOCN_PARAMS': 'uicode%3D20000174', 'SCF': 'Alhi3iuZKNjYbnL77hWoMvkQoE4edClB0S5n_r6NteS2u6p08-Xjk7DnXr_Em6P9tglZ1FZUg-UySu_l2XM-Ex8.', 'SUB': '_2A256-08sDeTxGeVP71sX8CjOwz6IHXVWBFFkrDV6PUJbkdBeLWLlkW02jdHGyTIL9-KM1daXjNac-RdsbA..', 'SSOLoginState': '1476345724'}, 'Accept-Encoding': 'gzip,deflate', 'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'User-Agent': 'Mozilla/5.0(X11;Linuxx86_64;rv', 'Upgrade-Insecure-Requests': '1', 'Host': 'm.weibo.cn'}")
     cookies = dict()
     ###################################
 
 
     page_source =str
     isLogined = bool
-    loginURL = "https://passport.weibo.cn/signin/login"
-    indexURL = "http://m.weibo.cn/"
-    messageURL = "http://m.weibo.cn/unread?t="
+    loginURL = u"https://passport.weibo.cn/signin/login"
+    indexURL = u"http://m.weibo.cn/"
+    messageURL = u"http://m.weibo.cn/unread?t="
     # searchURL = "http://m.weibo.cn/p/index?containerid=100103type=&q="
-    searchURL = "http://m.weibo.cn/container/getIndex?containerid=100103type"
+    searchURL = u"http://m.weibo.cn/container/getIndex?containerid=100103type"
     # funcs
 
     def set(self,bot_db:Bot_db):
@@ -75,7 +75,7 @@ class Bot:
             #文件名
             import datetime
             now = datetime.datetime.now()
-            filename = "[%s日%s时%s分]%s.htm" % (now.day,now.hour,now.minute,self.username)
+            filename = u"[%s日%s时%s分]%s.htm" % (now.day,now.hour,now.minute,self.username)
             #存储html
             with open(os.path.join(filepath,filename),'w') as f:
                 f.write(self.page_source)
@@ -125,7 +125,7 @@ class Bot:
         # if 'qp' in res and 'new' in res['qp']:
         #     print("账号[%s]有[%d]条新的首页消息." % (self.username,int(res['qp']['new'])))
         if 'ht' in res and 'sx' in res['ht']:
-            print("账号[%s]有[%d]条新的私信消息." % (self.username,int(res['ht']['sx'])))
+            print(u"账号[%s]有[%d]条新的私信消息." % (self.username,int(res['ht']['sx'])))
             return res['ht']['sx']
         # 返回未读私信数量
 
@@ -159,7 +159,7 @@ class Bot:
                 import json,datetime
                 res=json.loads(resp.text)
                 if(res['ok']==1):
-                    print("账号[%s]发送微博成功[%s]." % (self.username,datetime.datetime.now()))
+                    print(u"账号[%s]发送微博成功[%s]." % (self.username,datetime.datetime.now()))
 
         except Exception as e:
             print(e)
@@ -186,7 +186,7 @@ class Bot:
             )
             if (resp.status_code ==200):
                 import datetime
-                print("账号[%s]转发微博成功[%s]." % (self.username, datetime.datetime.now()))
+                print(u"账号[%s]转发微博成功[%s]." % (self.username, datetime.datetime.now()))
                 #记录下转发关系
                 one = TransmitedRelationship()
                 one.bot_id = self.username
@@ -211,7 +211,7 @@ class Bot:
             headers = self.headers
             headers.setdefault("Referer","http://m.weibo.cn/u/%s" %(uid))
             resp=self.session.post(
-                url="http://m.weibo.cn/attentionDeal/addAttention?",
+                url=u"http://m.weibo.cn/attentionDeal/addAttention?",
                 headers=headers,
                 cookies=self.cookies,
                 data=data,
@@ -219,7 +219,7 @@ class Bot:
             #无论是否已经关注，都返回关注
             if (resp.status_code ==200):
                  import datetime
-                 print("账号[%s]关注用户[%s]成功[%s]." % (self.username,uid,datetime.datetime.now()))
+                 print(u"账号[%s]关注用户[%s]成功[%s]." % (self.username,uid,datetime.datetime.now()))
         except Exception as e:
             print(e)
 
@@ -229,9 +229,9 @@ class Bot:
     #return:(uid,id,text)
     def Search(self,content:str):
         import urllib
-        queryStr = "=&q="+content
+        queryStr = u"=&q="+content
         queryStr = urllib.parse.quote(queryStr)
-        referURL = "http://m.weibo.cn/p/index?containerid=100103type"+queryStr
+        referURL = u"http://m.weibo.cn/p/index?containerid=100103type"+queryStr
         print(referURL)
         tmpHeaders = self.headers
         tmpHeaders.setdefault('Referer',referURL)
