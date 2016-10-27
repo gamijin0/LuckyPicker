7f2e81ecc8b66bfb6449814088c3441335b2381c
SleepyBadMan <1187692525@qq.com>
2016-10-27 20:05:19 +0800

    delete Redundant files

--- venv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/home/eric/PycharmProjects/LuckyPicker/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.8.15','django-admin.py'
-__requires__ = 'Django==1.8.15'
-__import__('pkg_resources').run_script('Django==1.8.15', 'django-admin.py')
